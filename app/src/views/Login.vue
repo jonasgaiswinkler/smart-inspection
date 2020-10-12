0b601229466373b6c43b1867ffb1eee5a12bba6e
jonasgaiswinkler <jonas.gaiswinkler@gmail.com>
2020-10-12 15:59:51 +0200

    add favicon add login

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,67 +1,107 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
+    <ion-header>
+      <ion-toolbar color="primary">
         <ion-title>smart inspection</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">{{ $t("login") }}</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        
-      </div>
+      <form class="height-100" id="login" @submit.stop.prevent="submit">
+        <ion-grid class="height-100">
+          <ion-row
+            color="primary"
+            class="ion-justify-content-center height-100"
+          >
+            <ion-col
+              class="ion-align-self-center"
+              size-md="6"
+              size-lg="5"
+              size-xs="12"
+            >
+              <div class="ion-text-center">
+                <h3>{{ $t("welcomeText") }}</h3>
+              </div>
+              <div class="ion-padding">
+                <ion-item>
+                  <ion-input
+                    v-model="email"
+                    name="email"
+                    type="email"
+                    :placeholder="$t('email')"
+                    required
+                  ></ion-input>
+                </ion-item>
+                <ion-item>
+                  <ion-input
+                    v-model="password"
+                    name="password"
+                    type="password"
+                    :placeholder="$t('password')"
+                    required
+                  ></ion-input>
+                </ion-item>
+              </div>
+              <div class="ion-padding">
+                <ion-button type="submit" expand="block">{{
+                  $t("login")
+                }}</ion-button>
+              </div>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </form>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { ref } from "vue";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonGrid,
+  IonCol,
+  IonItem,
+  IonInput,
+  IonButton,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonRow,
+    IonGrid,
+    IonCol,
+    IonItem,
+    IonInput,
+    IonButton,
+  },
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const submit = () => {
+      //
+    };
+    
+    return { email, password, submit };
+  },
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.height-100 {
+  height: 100%;
 }
 </style>
---
+++ app/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pwa: {
+    name: 'smart inspection',
+    themeColor: '#005096',
+    msTileColor: '#005096'
+  }
+}
